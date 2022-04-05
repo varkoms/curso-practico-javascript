55323b5e889175b2b183a7364e1655c758f777f3
VarkoMS <cmendozasoberanis@gmail.com>
2022-04-04 21:18:52 -0500

    Funcion generada calcularPrecioConDescuento()

--- descuentos.js
+++ descuentos.js
@@ -1,1 +1,11 @@
-console.log(`Test`);
+// Definir las variables
+const precioOriginal = 600;
+const descuento = 25;
+
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento
+  const precioConDescuento = precio * (porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
+}
+
+console.log(calcularPrecioConDescuento(3000, 15));
