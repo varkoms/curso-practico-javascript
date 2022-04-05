daeea86a2930e3d41427df45df0de9957b8f1d6d
VarkoMS <cmendozasoberanis@gmail.com>
2022-04-04 21:30:16 -0500

    Calculadora de precios con descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,14 @@
-// Definir las variables
-const precioOriginal = 600;
-const descuento = 25;
-
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento
   const precioConDescuento = precio * (porcentajePrecioConDescuento) / 100;
   return precioConDescuento;
 }
 
-console.log(calcularPrecioConDescuento(3000, 15));
+function priceDiscount() {
+  const inputPrice = document.getElementById("inputPrice").value;
+  const inputDiscount = document.getElementById("inputDiscount").value;
+  const precioConDescuento = calcularPrecioConDescuento(inputPrice, inputDiscount);
+
+  const resultPrice = document.getElementById("resultPrice");
+  resultPrice.innerText = `El precio con descuento son: $${precioConDescuento}`;
+}
